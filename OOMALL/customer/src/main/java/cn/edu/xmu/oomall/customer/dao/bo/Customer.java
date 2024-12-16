d10ac204ec812155fe01cc20f5fc93b8319f7587
Arceus234 <1921003016@qq.com>
2024-12-16 12:06:30 +0800

    wcf
    增加了customer的状态逻辑和deleteUser方法（有点问题有待研究）

--- OOMALL/customer/src/main/java/cn/edu/xmu/oomall/customer/dao/bo/Customer.java
+++ OOMALL/customer/src/main/java/cn/edu/xmu/oomall/customer/dao/bo/Customer.java
@@ -3,6 +3,10 @@ package cn.edu.xmu.oomall.customer.dao.bo;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.ToString;
 
 @Data
 public class Customer {
@@ -18,19 +22,79 @@ public class Customer {
     private LocalDateTime gmtModified;
 
     public Customer() {}
+    // 状态常量定义
+    public static final Byte NOBEDELETED = 0;     // 有效
+    public static final Byte BEDELETED = 1;     // 有效
+    @ToString.Exclude
+    @JsonIgnore
+    public static final Byte VALID = 0;     // 有效
+    @ToString.Exclude
+    @JsonIgnore
+    public static final Byte INVALID = 1;  // 停用
+    @ToString.Exclude
+    @JsonIgnore
+    public static final Byte DELETED = 2;  // 删除
 
+    // 状态名称映射
+    @ToString.Exclude
+    @JsonIgnore
+    public static final Map<Byte, String> STATUSNAMES = new HashMap<>() {
+        {
+            put(VALID, "有效");
+            put(INVALID, "停用");
+            put(DELETED, "删除");
+        }
+    };
+    @ToString.Exclude
+    @JsonIgnore
+    private static final Map<Byte, Set<Byte>> toInvalid = new HashMap<>() {
+        {
+            put(VALID, new HashSet<>() {
+                {
+                    add(INVALID);  // 有效 -> 停用
+                    add(DELETED);  // 有效 -> 删除
+                }
+            });
+            put(INVALID, new HashSet<>() {
+                {
+                    add(VALID);    // 停用 -> 有效
+                    add(DELETED);  // 停用 -> 删除
+                }
+            });
+            put(DELETED, new HashSet<>() {
+                // 删除状态不可迁移
+            });
+        }
+    };
+
+    /**
+     * 是否允许状态迁移
+     * @param newStatus 迁移去的状态
+     */
+    public boolean allowTransitInvalid(Byte newStatus) {
+        boolean ret = false;
+        assert (!Objects.isNull(this.invalid)) : String.format("对象(id=%d)的状态为null", this.getId());
+        Set<Byte> allowStatusSet = toInvalid.get(this.invalid);
+        if (!Objects.isNull(allowStatusSet)) {
+            ret = allowStatusSet.contains(newStatus);
+        }
+        return ret;
+    }
     public Customer(Long id, String userName, String name, Byte invalid) {
         this.id = id;
         this.userName = userName;
         this.name = name;
         this.invalid = invalid;
     }
-    public void setInvalid() {
-        if(this.invalid == 1){
-            this.invalid = 0;
+    public void setBeDelete() {
+            this.beDelete = BEDELETED; // 将 null 设置为 1
+    }
+    public void convertInvalid() {
+        if(this.invalid.equals(INVALID) ){
+            this.invalid = VALID;
         }
-        else if(this.invalid==0){
-            this.invalid = 1;
+        else if(this.invalid.equals(VALID)){
+            this.invalid = INVALID;
         }
         else {
 
