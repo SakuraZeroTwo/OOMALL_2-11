e21d17094c2698af1b7cc621d588292eaab07407
kodz9 <1482489777@qq.com>
2024-12-21 17:48:40 +0800

    CommentPo AuditPo
    添加评论po和审核记录Po

--- OOMALL/comment/src/main/java/cn/edu/xmu/oomall/comment/mapper/po/AuditPo.java
+++ OOMALL/comment/src/main/java/cn/edu/xmu/oomall/comment/mapper/po/AuditPo.java
@@ -1,4 +1,28 @@
 package cn.edu.xmu.oomall.comment.mapper.po;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comment_audit")
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class AuditPo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    public Long commentId;
+    public Long adminId;
+    public Byte auditResult;
+    public Long creatorId;
+    public String creatorName;
+    public Long modifierId;
+    public String modifierName;
+    public LocalDateTime gmtCreate;
+    public LocalDateTime gmtModified;
 }
--- OOMALL/comment/src/main/java/cn/edu/xmu/oomall/comment/mapper/po/CommentPo.java
+++ OOMALL/comment/src/main/java/cn/edu/xmu/oomall/comment/mapper/po/CommentPo.java
@@ -1,4 +1,32 @@
 package cn.edu.xmu.oomall.comment.mapper.po;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comment_comment")
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class CommentPo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    public String content;
+    public Long customerId;
+    public Long productId;
+    public Long orderId;
+    public int rating;
+    public Byte status;
+    public Byte appendStatus;
+    public Long creatorId;
+    public String creatorName;
+    public Long modifierId;
+    public String modifierName;
+    public LocalDateTime gmtCreate;
+    public LocalDateTime gmtModified;
 }
