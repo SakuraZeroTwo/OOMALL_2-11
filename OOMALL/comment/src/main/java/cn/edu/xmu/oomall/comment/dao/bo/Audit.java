f44d680b5721bf32992cbcd1d150c5af32a3c8b4
Arceus234 <1921003016@qq.com>
2024-12-21 16:16:52 +0800

    增加了Audit的bo对象

--- OOMALL/comment/src/main/java/cn/edu/xmu/oomall/comment/dao/bo/Audit.java
+++ OOMALL/comment/src/main/java/cn/edu/xmu/oomall/comment/dao/bo/Audit.java
@@ -1,4 +1,32 @@
 package cn.edu.xmu.oomall.comment.dao.bo;
+import cn.edu.xmu.javaee.core.model.bo.OOMallObject;
+import lombok.Data;
 
-public class Audit {
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import static cn.edu.xmu.javaee.core.model.Constants.MAX_RETURN;
+public class Audit extends OOMallObject implements Serializable{
+    private Long commentId;
+    private Long adminId;
+    private Byte auditResult;
+    
+
+
+    @Override
+    public void setGmtCreate(LocalDateTime gmtCreate) {
+        this.gmtCreate=gmtCreate;
+    }
+
+    @Override
+    public void setGmtModified(LocalDateTime gmtModified) {
+        this.gmtModified=gmtModified;
+    }
 }
